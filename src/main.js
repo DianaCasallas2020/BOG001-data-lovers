f5f1dc05bb21ec3b74a390669515488a4d72d2fa
Diana <dianacasallas2020@gmail.com>
2020-07-09 18:40:00 -0500

    responsive index

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+window.onload = function results (allCharacters){
+    let allData = data.results;
+    let card = ''
+         for ( let i = 0;  i < allData.length; i++) {
+            card += `
+            <li class= card>
+            <h2 class="name">${allData[i].name}</h2>
+            <img class ="image_pokemon" src="${allData[i].image}" alt="rick-image">
+            <button class="btn_aboutMe">About me</button> 
+            `
+       };
+       document.getElementById('listOfPokemon').innerHTML = card;
+      };
 
 console.log(example, data);
