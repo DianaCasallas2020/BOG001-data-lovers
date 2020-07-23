59776c8886531f8f02df82c94c8d9eb08380a94e
Diana <dianacasallas2020@gmail.com>
2020-07-23 12:50:38 -0500

    barra de busqueda lista

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-//import { example } from './data.js';
+//import { orderNames } from './data.js';
 
-const invocation = new XMLHttpRequest();
 const fileCharacter = document.getElementById('fileCharacters');
 const fileEpisodes = document.getElementById('fileEpisodes');
+const searchBar = document.getElementById('searchBar');
+let allCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+   const searchString = e.target.value.toLowerCase();
+   console.log(searchString);
+   const filterCharacters = allCharacters.filter(character => {
+      return character.name.toLowerCase().includes(searchString) ||
+      character.species.toLowerCase().includes(searchString) ||
+      character.status.toLowerCase().includes(searchString) ||
+      character.origin.toLowerCase().includes(searchString)||
+      character.location.toLowerCase().includes(searchString)
+   });
+   displayCharacters(filterCharacters);
+});
 
 async function get(url) {
    try {
@@ -10,54 +24,55 @@ async function get(url) {
       return await data.json()
    } catch (error) {
     console.log(`error con el servicio ${url}`);
-   }
-}
+   };
+};
 
 const fetchCharacters = async () => {
   const promises = [];
    for (let i = 1; i < 494; i++){
       const url = `https://rickandmortyapi.com/api/character/${i}`;
    promises.push(fetch(url).then(res => res.json()));
-}
+};
 
       Promise.all(promises).then(results => {
-         const allCharacters = results.map((data, index) => ({
+         
+         allCharacters = results.map( data => ({
             name: data.name,
-            id: index + 1,
+            id: data.id,
             image: data.image,
             species: data.species,
             episode: data.episode,
+            status: data.status,
+            origin: data.origin.name,
+            location: data.location.name,
          }));
+         console.log(allCharacters);
          displayCharacters(allCharacters);
       });
 
 };
 
 const displayCharacters = (allCharacters) =>{
-   console.log(allCharacters);
    const characterHTMLString = allCharacters.map ((allPersonage) =>
 
    `<li class="cont03" onclick="selectCharacter(${allPersonage.id})">
       <img class="imgCharacteres" src="${allPersonage.image}" alt="rick-and-morty-image">
       <h2 class="name">${allPersonage.name}</h2>
-   </li>`
-
-   )
+   </li>`)
 
    .join('');
    
-      fileCharacter.innerHTML = characterHTMLString;
+   fileCharacter.innerHTML = characterHTMLString;
 
 };
-
-
 
 window.selectCharacter = async (id) => {
    const url = `https://rickandmortyapi.com/api/character/${id}`;
    const res = await fetch(url);
    const allPersonage = await res.json();
+      
    loadModal(allPersonage);
-}
+};
 
 const loadModal = (allPersonage) => {
    const htmlString = `
@@ -72,18 +87,17 @@ const loadModal = (allPersonage) => {
                   <img class="imgCharacteres" src="${allPersonage.image}" alt="rick-and-morty-image">
                </article>
                <article class="infoPersonaje">
-                  <p class="textModal"><small>Status: </small><br>${allPersonage.status}</p>
-                  <p class="textModal"><small>Specie: </small><br>${allPersonage.species}</p>
-                  <p class="textModal"><small>Gender: </small><br>${allPersonage.gender}</p>
-                  <p class="textModal"><small>First seen in: </small><br>${allPersonage.origin.name}</p>
-                  <p class="textModal"><small>Last known location: </small><br>${allPersonage.location.name}</p>
-                  <p class="text"><small>Episodes: </small><br>${allPersonage.episode}</p>
+                  <p class="textModal"><small>Status </small><br>${allPersonage.status}</p>
+                  <p class="textModal"><small>Specie </small><br>${allPersonage.species}</p>
+                  <p class="textModal"><small>Gender </small><br>${allPersonage.gender}</p>
+                  <p class="textModal"><small>First seen in </small><br>${allPersonage.origin.name}</p>
+                  <p class="textModal"><small>Last known location </small><br>${allPersonage.location.name}</p>
                </article>
+               <button class="btnEpisodios" onclick="()">Episodes</button>
             </div>
          </div>
       </section>`;
       fileCharacter.innerHTML = htmlString + fileCharacter.innerHTML;
-      console.log(htmlString);   
 };
 
 window.closeModal = async () => {
@@ -91,4 +105,7 @@ window.closeModal = async () => {
    modal.parentElement.removeChild(modal);
 };
 
+
+
 fetchCharacters();
+
